be6911f59be85ac0258ac36cdd366e3e09c43da1
dnjeh(school) <ehdnj.001@gmail.com>
2022-08-26 12:00:04 +0900

    yahhho!

--- whilepractice6.c
+++ whilepractice6.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 int main() {
-    printf("\n에제14==========\n");
-    int n,sum=0;
-    printf("정수 입력(0이면 종료) : ");
+    /*printf("\n에제14==========\n");
+    int n=0,sum=0;
+    printf("정수 입력(음수면 종료) : ");
     do{ 
-        scanf("%d", &n);
         sum+=n;
-    } while(n);
-    printf("합 : %d", sum);
+        scanf("%d", &n);
+    } while(n>=0);
+    printf("합 : %d", sum);*/
+    printf("\n에제15==========\n");
+    int i, num;
+
+    i=1;
+    printf("약수를 구할 수 입력 : ");
+    scanf("%d", &num);
+    while(i<=num) {
+        if(num%i==0) {
+            printf("%d ", i);
+        }
+        i++;
+    }
 }
