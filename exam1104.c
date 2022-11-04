0f9d597ddb5e6cb8d98318cb85e8f79fff1a6ead
dnjeh(school) <ehdnj.001@gmail.com>
2022-11-04 13:43:25 +0900

    Today's afternoon's clas's

--- exam1104.c
+++ exam1104.c
@@ -72,7 +72,16 @@ void exam6(){
 }
 
 void exam7(){
+	int h[] = {170, 178, 175, 180, 177, 167, 189};
+	int i, h_min = h[0], num;
 
+	for(i=0;i<7;i++) {
+		if(h_min>h[i]) {
+			h_min=h[i];
+			num=i+1;
+		}
+	}
+	printf("가장 키가 작은 학생 번호 = %d번, 키 = %dcm \n", num, h_min);
 }
 
 void exam8(){
